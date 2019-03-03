66744a373202bb89ac4ce0b256ec453f38c9bea8
Karolina Blümke <karolina.blumke@gmail.com>
2019-03-03 15:26:24 +0100

    Add Label changes and styling

--- app/frontend/src/Label/Label.js
+++ app/frontend/src/Label/Label.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import './Label.css'
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-
-class Label extends Component {
+class Label extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -16,27 +19,54 @@ class Label extends Component {
             type: '',
             favorite: false,
     };
+    }
 
-render() {
+    componentWillMount() {
+        const { image, title, year, imdbID, type, favorite } = this.props;
+        this.setState({ image, title, year, imdbID, type, favorite });
+    }
+
+    render() {
     const { image, title, year, imdbID, type, favorite } = this.state;
 
-    return (
-        <Container>
-            <title>{title}</title>
-            <Image src={image}/>
-            <Icon
-                name={favorite ? 'star' : 'star-o'}
-                color={favorite ? '#FFD700' : 'rgb(255,215,0)'}
-                size={30}
-                style={{ marginBottom: 10, marginTop: 20 }}
-                onPress={() => this.setState({ favorite: !favorite })}
-            />
-            <p>{ year }</p>
-            <p>{ imdbID }</p>
-            <p>{ type }</p>
-            {favorite ? 'Remove from favorite' : 'Add to favorite'}
-        </Container>
-    )
+        return (
+            <div className="Label">
+                <FontAwesomeIcon
+                    name={favorite ? 'star' : 'star-o'}
+                    color={favorite ? '#FFD700' : 'rgb(255,215,0)'}
+                    size={30}
+                    style={{ marginBottom: 10, marginTop: 20 }}
+                    onPress={() => this.setState({ favorite: !favorite })}
+                />
+                <Container>
+                <Row>
+                    <Col xs={6} md={4}>
+                        <Image src={image} />
+                    </Col>
+                    <Col xs={12} md={8}>
+                        <ListGroup>
+                            <ListGroup.Item><b>Title:</b> { title }</ListGroup.Item>
+                            <ListGroup.Item><b>Year of production:</b> { year }</ListGroup.Item>
+                            <ListGroup.Item><b>imdbID:</b> { imdbID }</ListGroup.Item>
+                            <ListGroup.Item><b>Type:</b> { type }</ListGroup.Item>
+                            <ListGroup.Item>{favorite ? 'Remove from favorite' : 'Add to favorite'}</ListGroup.Item>
+                        </ListGroup>
+                        <FontAwesomeIcon
+                            name={favorite ? 'star' : 'star-o'}
+                            color={favorite ? '#FFD700' : 'rgb(255,215,0)'}
+                            size={30}
+                            style={{ marginBottom: 10, marginTop: 20 }}
+                            onPress={() => this.setState({ favorite: !favorite })}
+                        />
+                    </Col>
+                </Row>
+                </Container>
+            </div>
+
+                
+
+
+        )
 }
 }
 
