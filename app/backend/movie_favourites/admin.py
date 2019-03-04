396ff8ce846e8fcd264c9407cacc73f77d24758a
Karolina Blümke <karolina.blumke@gmail.com>
2019-03-05 00:21:07 +0100

    Add new model and register it on admin

--- app/backend/movie_favourites/admin.py
+++ app/backend/movie_favourites/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from movie_favourites.models import  Film
 
-# Register your models here.
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'year',
+        'imdbID',
+        'movie_type',
+        'poster_url',
+    )
+    search_fields = ('title',)
+
+admin.site.register(Film, FilmAdmin)
+
--- app/backend/movie_favourites/models.py
+++ app/backend/movie_favourites/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    """
+    Films model
+    """
+    MOVIE = 'movie' 
+    SERIES = 'series'
+    EPISODE = 'episode'
+    GAME = 'game'
+    TYPE_CHOICES = (
+        (MOVIE, 'movie'),
+        (SERIES, 'series'),
+        (EPISODE, 'episode'),
+        (GAME, 'game'))
+
+    title = models.CharField(max_length=128, unique=True)
+    year = models.CharField(max_length=4)
+    imdbID = models.CharField(max_length=12)
+    movie_type = models.PositiveIntegerField(choices=TYPE_CHOICES)
+    poster_url = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.title
+
+
+        
