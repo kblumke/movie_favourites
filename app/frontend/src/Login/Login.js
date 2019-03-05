d762a4f97fb833b079e9934b6d10d625647ae2bd
Karolina Blümke <karolina.blumke@gmail.com>
2019-03-05 22:30:21 +0100

    Add Login component

---
+++ app/frontend/src/Login/Login.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+  
+    // Using a class based component here because we're accessing DOM refs
+   
+    handleSignIn(e) {
+      e.preventDefault()
+      let username = this.refs.username.value
+      let password = this.refs.password.value
+      this.props.onSignIn(username, password)
+    }
+    
+    render() {
+      return (
+        <div>
+        <form onSubmit={this.handleSignIn.bind(this)}>
+          <h3>Sign in</h3>
+          <input type="text" ref="username" placeholder="enter you username" />
+          <input type="password" ref="password" placeholder="enter password" />
+          <input type="submit" value="Login" />
+        </form>
+        </div>
+      )
+    }
+  
+  }
+
+  export default Login;
